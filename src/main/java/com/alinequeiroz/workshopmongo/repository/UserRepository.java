b1a84caf4394c16bebc13bea3b1721340006f669
alcampel <aline.queiroz@capgemini.com>
2023-03-28 11:54:48 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alinequeiroz/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alinequeiroz.workshopmongo.repository;
+
+import com.alinequeiroz.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
